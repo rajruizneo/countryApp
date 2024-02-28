e1f208a37d790dcdf451cb93d7f2090000bc476c
Francisco Ruiz <rajesh.ruiz.neoris@gmail.com>
2024-02-28 10:37:58 -0600

    avance curso

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css'
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
 
-}
+  public country?:Country;
+
+  constructor(private activatedRoute:ActivatedRoute, private countryService:CountriesService
+              ,private router:Router
+            ){}
+
+
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.countryService.serachCountryByAlphaCode(id))
+    )
+    .subscribe( (resp) => {
+      console.log({resp});
+      if (!resp) { return this.router.navigateByUrl(''); }
+      console.log('Existe Pais');
+      this.country = resp[0];
+      return this.country;
+    });
+    }
+
+    searchCountry(code:string){
+      this.countryService.serachCountryByAlphaCode(code)
+        .subscribe( country => {
+          console.log({country})
+        });
+    }
+
+  }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -8,6 +8,16 @@ export class CountriesService {
 
   private url:string='https://restcountries.com/v3.1'
   constructor(private httpClient: HttpClient) { }
+
+  serachCountryByAlphaCode(term:string):Observable<Country[] | null>{
+    const url=`${this.url}/alpha/${term}`;
+
+    return this.httpClient.get<Country[]>(url)
+      .pipe(
+        //map(countries => countries.length>0? countries[0]:null),
+        catchError(()=> of (null))
+      );
+  }
 
   serachCapital(term:string):Observable<Country[]>{
     // const params:HttpParams = new HttpParams()
